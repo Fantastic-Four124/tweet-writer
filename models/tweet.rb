8ba630228bcfe5ed635acf3b2b5d900461e5990e
vagrant <vagrant@precise64.(none)>
2018-03-24 20:49:24 +0000

    Ironed out bugs; basic functionality achieved

--- models/tweet.rb
+++ models/tweet.rb
@@ -4,11 +4,11 @@ class Tweet
 
   field :contents, type: String
   field :date_posted, type: DateTime
-  field :user_id, type: Integer
+  field :user_id, type: String
   field :hashtags, type: Array
   field :mentions, type: Array
 
   attr_readonly :user_id, :date_posted, :contents
   validates :user_id, presence: true
-  store_in collection: 'tweets'
+  store_in collection: 'nt-tweets'
 end
--- service.rb
+++ service.rb
@@ -1,6 +1,8 @@
 # server.rb
 require 'sinatra'
 require 'mongoid'
+require 'byebug'
+require 'json'
 require_relative 'models/tweet'
 
 # DB Setup
@@ -10,21 +12,29 @@ Mongoid.load! "mongoid.config"
 enable :sessions
 
 set :bind, '0.0.0.0' # Needed to work with Vagrant
-set :port, 8080
+set :port, 8085
 
 # These are still under construction.
 
 post '/api/v1/tweets/new' do
   result = Hash.new
   tweet = Tweet.new(
-    contents: params[:tweet],
-    user_id: session[:user_id],
+    contents: params[:contents],
+    user_id: params[:user_id],
     date_posted: Time.now,
     hashtags: JSON.parse(params[:hashtags]),
     mentions: JSON.parse(params[:mentions])
   )
+  #byebug
   saved = tweet.save
-  byebug
+  #byebug
   result[:saved] = saved
   result.to_json
+end
+
+# ONLY TO BE USED FOR TESTING
+post '/api/v1/tweets/delete' do
+  success = Tweet.where(_id: params[:id]).delete
+  byebug
+  success.to_json
 end
