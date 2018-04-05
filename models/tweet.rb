871a044ee525ef4b706d81447619a1292bae5390
vagrant <vagrant@precise64.(none)>
2018-04-05 22:20:09 +0000

    added basic search functionality

--- models/tweet.rb
+++ models/tweet.rb
@@ -1,14 +1,17 @@
 class Tweet
   include Mongoid::Document
   include Mongoid::Timestamps
+  include Mongoid::Search
 
   field :contents, type: String
   field :date_posted, type: DateTime
+  field :username, type: String
   field :user_id, type: String
   field :hashtags, type: Array
   field :mentions, type: Array
 
-  attr_readonly :user_id, :date_posted, :contents
-  validates :user_id, presence: true
+  attr_readonly :username, :user_id, :date_posted, :contents
+  validates :username, :user_id, presence: true
+  search_in :contents, :hashtags
   store_in collection: 'nt-tweets'
 end
--- service.rb
+++ service.rb
@@ -1,6 +1,7 @@
 # server.rb
 require 'sinatra'
 require 'mongoid'
+require 'mongoid_search'
 require 'byebug'
 require 'json'
 require_relative 'models/tweet'
@@ -17,26 +18,23 @@ set :port, 8085
 # These are still under construction.
 
 post '/api/v1/tweets/new' do
-  puts ENV["MONGOUSER"]
-  puts ENV["MONGOPASS"]
   result = Hash.new
   tweet = Tweet.new(
     contents: params[:contents],
+    username: params[:username],
     user_id: params[:user_id],
     date_posted: Time.now,
     hashtags: JSON.parse(params[:hashtags]),
     mentions: JSON.parse(params[:mentions])
   )
-  #byebug
   saved = tweet.save
-  #byebug
   result[:saved] = saved
   result.to_json
 end
 
 # ONLY TO BE USED FOR TESTING
-post '/api/v1/tweets/delete' do
-  success = Tweet.where(_id: params[:id]).delete
-  byebug
-  success.to_json
-end
+# post '/api/v1/tweets/delete' do
+#   success = Tweet.where(_id: params[:id]).delete
+#   byebug
+#   success.to_json
+# end
